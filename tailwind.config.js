0735a4e4af5aac1a6bcf94536c7944d0e4caa029
tlc3004 <leonantoniobm@gmail.com>
2025-08-04 21:58:53 -0500

    wuau

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}"
-  ],
+  content: [],
   theme: {
-    extend: {
-      colors: {
-        // colores personalizados si quieres
-        'tumaki': '#FF4F5A',
-        'garage': '#1A1A1A',
-      },
-      fontFamily: {
-        'display': ['Poppins', 'sans-serif'],
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
+
